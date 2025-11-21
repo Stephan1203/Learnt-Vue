e8e3d62a5e41f0248f30f344fbb0d62764143a18
Sergei <38001255+sersha111@users.noreply.github.com>
2025-11-21 10:54:32 +0300

    delete fun

--- src/components/user.vue
+++ src/components/user.vue
@@ -2,7 +2,7 @@
 import { ref } from "vue";
 
 let id = ref(1)
-let users = ref([])
+let todo_list = ref([])
 let userName = ref("")
 let date = ref("")
 let todo = ref("")
@@ -14,7 +14,7 @@ function addUser() {
     return
   }
   isInputsEmpty.value = false
-  users.value.push({
+  todo_list.value.push({
     id: id.value++,
     userName: userName.value,
     date: date.value,
@@ -23,6 +23,11 @@ function addUser() {
   userName.value = "";
   date.value = "";
   todo.value = "";
+}
+
+function delete_list() {
+  todo_list.value = []
+  id.value = 1
 }
 </script>
 
@@ -44,26 +49,22 @@ function addUser() {
         <label for="todo">Задача</label>
       </div>
       <button @click="addUser">Добавить задачу</button>
-      <p v-if="isInputsEmpty" class="is_empty" :class="{"empty-list"}">Заполните все поля</p>
+      <p v-if="isInputsEmpty" class="is_empty">Заполните все поля</p>
     </div>
 
 
     <div class="user_list">
-      <div v-if="users.length > 0">
+      <div v-if="todo_list.length > 0">
         <ul>
-          <li v-for="todo in users" :key="todo.id">
-            <p class="todo_info">
-              {{ todo.id }}. {{ todo.userName }} {{ todo.date }}
-            </p>
+          <li v-for="todo in todo_list" :key="todo.id">
+            <p class="todo_info">{{ todo.id }}. {{ todo.userName }} {{ todo.date }}</p>
             <p class="todo_item">{{ todo.todo }}</p>
           </li>
         </ul>
-        <button>
-          <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 110.61 122.88" height="30px" width="30px">
-            <title>Delete all</title>
-            <path d="M39.27,58.64a4.74,4.74,0,1,1,9.47,0V93.72a4.74,4.74,0,1,1-9.47,0V58.64Zm63.6-19.86L98,103a22.29,22.29,0,0,1-6.33,14.1,19.41,19.41,0,0,1-13.88,5.78h-45a19.4,19.4,0,0,1-13.86-5.78l0,0A22.31,22.31,0,0,1,12.59,103L7.74,38.78H0V25c0-3.32,1.63-4.58,4.84-4.58H27.58V10.79A10.82,10.82,0,0,1,38.37,0H72.24A10.82,10.82,0,0,1,83,10.79v9.62h23.35a6.19,6.19,0,0,1,1,.06A3.86,3.86,0,0,1,110.59,24c0,.2,0,.38,0,.57V38.78Zm-9.5.17H17.24L22,102.3a12.82,12.82,0,0,0,3.57,8.1l0,0a10,10,0,0,0,7.19,3h45a10.06,10.06,0,0,0,7.19-3,12.8,12.8,0,0,0,3.59-8.1L93.37,39ZM71,20.41V12.05H39.64v8.36ZM61.87,58.64a4.74,4.74,0,1,1,9.47,0V93.72a4.74,4.74,0,1,1-9.47,0V58.64Z"/>
-          </svg>
-        </button>
+        <svg @click="delete_list" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 110.61 122.88" height="30px" width="30px">
+          <title>Delete all</title>
+          <path d="M39.27,58.64a4.74,4.74,0,1,1,9.47,0V93.72a4.74,4.74,0,1,1-9.47,0V58.64Zm63.6-19.86L98,103a22.29,22.29,0,0,1-6.33,14.1,19.41,19.41,0,0,1-13.88,5.78h-45a19.4,19.4,0,0,1-13.86-5.78l0,0A22.31,22.31,0,0,1,12.59,103L7.74,38.78H0V25c0-3.32,1.63-4.58,4.84-4.58H27.58V10.79A10.82,10.82,0,0,1,38.37,0H72.24A10.82,10.82,0,0,1,83,10.79v9.62h23.35a6.19,6.19,0,0,1,1,.06A3.86,3.86,0,0,1,110.59,24c0,.2,0,.38,0,.57V38.78Zm-9.5.17H17.24L22,102.3a12.82,12.82,0,0,0,3.57,8.1l0,0a10,10,0,0,0,7.19,3h45a10.06,10.06,0,0,0,7.19-3,12.8,12.8,0,0,0,3.59-8.1L93.37,39ZM71,20.41V12.05H39.64v8.36ZM61.87,58.64a4.74,4.74,0,1,1,9.47,0V93.72a4.74,4.74,0,1,1-9.47,0V58.64Z"/>
+        </svg>
       </div>
       <p v-else class="empty-list">Список пустой</p>
     </div>
@@ -124,13 +125,12 @@ input {
   border-radius: 8px;
   border: solid 1px #8d8d8d;
   box-sizing: border-box;
-  transition: all 0.3s ease;
+  transition: all 0.5s ease;
   height: 100%;
 }
 
-input:hover,
-input:focus {
-  box-shadow: 0 1px 5px #8d8d8d;
+.block_1 input {
+  max-width: 150px;
 }
 
 .block_1 input:focus ~ label,
@@ -222,8 +222,12 @@ input:not(:placeholder-shown) {
   font-style: italic;
 }
 
-.user_list button {
-  border: none;
-  background: none;
+.user_list svg {
+  transition: all 0.1s ease;
+}
+
+.user_list svg:hover {
+  cursor: pointer;
+  transform: scale(1.05);
 }
 </style>
