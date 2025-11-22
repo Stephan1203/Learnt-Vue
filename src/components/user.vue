e912d6844f736db98f4b75d494a9018703b84f72
Sergei <38001255+sersha111@users.noreply.github.com>
2025-11-22 18:45:12 +0300

    corect index

--- src/components/user.vue
+++ src/components/user.vue
@@ -1,20 +1,20 @@
 <script setup>
 import { ref } from "vue";
 
-let id = ref(1)
-let todo_list = ref([])
-let userName = ref("")
-let date = ref("")
-let todo = ref("")
-let isInputsEmpty = ref(false)
+const id = ref(1)
+const todoList = ref([])
+const userName = ref("")
+const date = ref("")
+const todo = ref("")
+const isInputsEmpty = ref(false)
 
-function addUser() {
+function addTodo() {
   if (!userName.value.trim() || !date.value.trim() || !todo.value.trim()) {
     isInputsEmpty.value = true
     return
   }
   isInputsEmpty.value = false
-  todo_list.value.push({
+  todoList.value.push({
     id: id.value++,
     userName: userName.value,
     date: date.value,
@@ -25,9 +25,13 @@ function addUser() {
   todo.value = "";
 }
 
-function delete_list() {
-  todo_list.value = []
+function deleteList() {
+  todoList.value = []
   id.value = 1
+}
+
+function deleleTodo(todo) {
+  todoList.value = todoList.value.filter((t) => t !== todo)
 }
 </script>
 
@@ -48,20 +52,23 @@ function delete_list() {
         <input v-model="todo" id="todo" type="text" placeholder="" @focus="isInputsEmpty = false" autocomplete="off"/>
         <label for="todo">Задача</label>
       </div>
-      <button @click="addUser">Добавить задачу</button>
+      <button @click="addTodo">Добавить задачу</button>
       <p v-if="isInputsEmpty" class="is_empty">Заполните все поля</p>
     </div>
 
 
     <div class="user_list">
-      <div v-if="todo_list.length > 0">
+      <div v-if="todoList.length > 0">
         <ul>
-          <li v-for="todo in todo_list" :key="todo.id">
-            <p class="todo_info">{{ todo.id }}. {{ todo.userName }} {{ todo.date }}</p>
-            <p class="todo_item">{{ todo.todo }}</p>
+          <li v-for="(todo, index) in todoList" :key="todo.id">
+            <p class="todo_info">{{ index + 1 }}. {{ todo.userName }} {{ todo.date }}</p>
+          <p>
+            <button @click="deleleTodo(todo)">X</button>
+            <span class="todo">{{ todo.todo }}</span>
+          </p>
           </li>
         </ul>
-        <svg @click="delete_list" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 110.61 122.88" height="30px" width="30px">
+        <svg @click="deleteList" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 110.61 122.88" height="30px" width="30px">
           <title>Delete all</title>
           <path d="M39.27,58.64a4.74,4.74,0,1,1,9.47,0V93.72a4.74,4.74,0,1,1-9.47,0V58.64Zm63.6-19.86L98,103a22.29,22.29,0,0,1-6.33,14.1,19.41,19.41,0,0,1-13.88,5.78h-45a19.4,19.4,0,0,1-13.86-5.78l0,0A22.31,22.31,0,0,1,12.59,103L7.74,38.78H0V25c0-3.32,1.63-4.58,4.84-4.58H27.58V10.79A10.82,10.82,0,0,1,38.37,0H72.24A10.82,10.82,0,0,1,83,10.79v9.62h23.35a6.19,6.19,0,0,1,1,.06A3.86,3.86,0,0,1,110.59,24c0,.2,0,.38,0,.57V38.78Zm-9.5.17H17.24L22,102.3a12.82,12.82,0,0,0,3.57,8.1l0,0a10,10,0,0,0,7.19,3h45a10.06,10.06,0,0,0,7.19-3,12.8,12.8,0,0,0,3.59-8.1L93.37,39ZM71,20.41V12.05H39.64v8.36ZM61.87,58.64a4.74,4.74,0,1,1,9.47,0V93.72a4.74,4.74,0,1,1-9.47,0V58.64Z"/>
         </svg>
@@ -205,15 +212,11 @@ input:not(:placeholder-shown) {
 .user_list ul {
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  gap: 15px;
 }
 
 .todo_info {
   font-weight: bold;
-}
-
-.todo_item {
-  padding-left: 10px;
 }
 
 .empty-list {
@@ -229,5 +232,17 @@ input:not(:placeholder-shown) {
 .user_list svg:hover {
   cursor: pointer;
   transform: scale(1.05);
+}
+
+.user_list li button {
+  background: none;
+  border: 2px solid #c2c2c2;
+  padding: 2px 3px;
+  border-radius: 5px;
+  margin-right: 10px;
+}
+
+.user_list li button:hover {
+  cursor: pointer;
 }
 </style>
